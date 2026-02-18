e0a284768f7c78c5823706b9ae249dc4e2091ede
ryeno-beetle <wingytinker@icloud.com>
2026-02-18 15:34:39 -0800

    finished

--- src/scenes/Title.js
+++ src/scenes/Title.js
@@ -19,15 +19,17 @@ class Title extends Phaser.Scene {
         this.load.image('theseus', 'img/theseus.png')
         this.load.image('lysander', 'img/lysander.png')
         this.load.image('philostrate', 'img/philostrate.png')
+        this.load.image('puppycat', 'img/puppycat.png');
 
         // load bitmap font
         this.load.bitmapFont('gem_font', 'font/gem.png', 'font/gem.xml')
+        this.load.bitmapFont('k_font', 'font/kindergarten.png', 'font/kindergarten.xml')
     }
 
     create() {
         // add title text
-        this.add.bitmapText(centerX, centerY - 32, 'gem_font', 'A MIDSUMMER NIGHT\'S DREAM', 32).setOrigin(0.5)
-        this.add.bitmapText(centerX, centerY + 16, 'gem_font', 'Press SPACE to start', 24).setOrigin(0.5)
+        this.add.bitmapText(centerX, centerY - 32, 'k_font', 'A MIDSUMMER NIGHT\'S DREAM', 40).setOrigin(0.5)
+        this.add.bitmapText(centerX, centerY + 16, 'k_font', 'Press SPACE to start', 30).setOrigin(0.5)
 
         // create input
         cursors = this.input.keyboard.createCursorKeys()
